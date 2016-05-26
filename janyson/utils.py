f5f2410b99ddf98cf839fe736566e83f5c76f28b
un.def <un.def@ya.ru>
2016-05-26 22:27:54 +0300

    validators tests; utils no cover

--- janyson/utils.py
+++ janyson/utils.py
@@ -10,9 +10,10 @@ def setdefaultattr(obj, name, value):
 
 def _get_attrs(obj):
     from types import DictProxyType
-    if not hasattr(obj, '__dict__'):   # slots only
-        return []
-    if not isinstance(obj.__dict__, (dict, DictProxyType)):
+    if not hasattr(obj, '__dict__'):   # pragma: no cover
+        return []   # slots only
+    if not isinstance(obj.__dict__,
+                      (dict, DictProxyType)):   # pragma: no cover
         raise TypeError("{}.__dict__ is not a dictionary".format(
             obj.__name__))
     return obj.__dict__.keys()
@@ -24,7 +25,7 @@ def dir_py2(obj):
     attrs = set()
     if not hasattr(obj, '__bases__'):   # obj is an instance
         if not hasattr(obj, '__class__'):   # slots
-            return sorted(_get_attrs(obj))
+            return sorted(_get_attrs(obj))   # pragma: no cover
         cls = obj.__class__
         attrs.update(_get_attrs(cls))
     else:   # obj is a class
