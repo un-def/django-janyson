d9f7ab66ceea95e76431abdf8295a5b9ceadf283
un.def <un.def@ya.ru>
2016-05-27 14:26:53 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+try:
+    import pypandoc
+    long_description = pypandoc.convert(long_description, 'rst', 'md')
+    long_description = long_description.replace('\r', '')
+    with open('README.rst', mode='w', encoding='utf-8') as f:
+        f.write(long_description)
+except (ImportError, OSError):
+    print("!!! Can't convert README.md - install pandoc and/or pypandoc.")
+
+
+setup(
+    name='django-janyson',
+    version='0.1.0',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=[
+        'django>=1.9',
+        'six',
+    ],
+    license='BSD License',
+    description='Virtual model fields that are transparently '
+                'mapped to Postgres jsonb',
+    long_description=long_description,
+    url='https://github.com/un-def/django-janyson',
+    author='un.def',
+    author_email='un.def@ya.ru',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
